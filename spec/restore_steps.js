72b7daee97af00f5618407738aae2c335445b9af
James Coglan <jcoglan@gmail.com>
2012-08-02 13:29:42 +0100

    Refactor request code in tests.

--- spec/restore_steps.js
+++ spec/restore_steps.js
@@ -2,6 +2,36 @@ var Restore = require("../lib/restore"),
     http    = require("http"),
     qs      = require("querystring")
 
+var request = function(self, host, port, method, path, params, callback) {
+  var options = {
+    host:     host,
+    port:     port,
+    method:   method,
+    path:     path
+  }
+  var query = qs.stringify(params)
+  
+  if (method === "POST" || method === "PUT")
+    options.headers = {"Content-Type": "application/x-www-form-urlencoded"}
+  else if (query)
+    options.path += '?' + query
+  
+  var request = http.request(options, function(response) {
+    var body = ""
+    response.addListener("data", function(c) { body += c.toString() })
+    response.addListener("end", function() {
+      response.body = body
+      self.response = response
+      callback()
+    })
+  })
+  
+  if (method === "POST" || method === "PUT")
+    request.write(query)
+  
+  request.end()
+}
+
 module.exports = JS.Test.asyncSteps({
   start: function(port, callback) {
     this._server = new Restore({store: this.store})
@@ -16,37 +46,11 @@ module.exports = JS.Test.asyncSteps({
   },
   
   get: function(path, params, callback) {
-    var self = this
-    http.get({host: "localhost", port: this._port, path: path}, function(response) {
-      var body = ""
-      response.addListener("data", function(c) { body += c.toString() })
-      response.addListener("end", function() {
-        response.body = body
-        self.response = response
-        callback()
-      })
-    })
+    request(this, "localhost", this._port, "GET", path, params, callback)
   },
   
   post: function(path, params, callback) {
-    var self = this
-    var request = http.request({
-      host:     "localhost",
-      port:     this._port,
-      method:   "POST",
-      path:     path,
-      headers:  {"Content-Type": "application/x-www-form-urlencoded"}
-    }, function(response) {
-      var body = ""
-      response.addListener("data", function(c) { body += c.toString() })
-      response.addListener("end", function() {
-        response.body = body
-        self.response = response
-        callback()
-      })
-    })
-    request.write(qs.stringify(params))
-    request.end()
+    request(this, "localhost", this._port, "POST", path, params, callback)
   },
   
   check_status: function(status, callback) {
