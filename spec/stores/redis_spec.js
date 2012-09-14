55fd7b490c42f7878f2b99149d7103a5060dd041
James Coglan <jcoglan@gmail.com>
2012-09-14 18:14:16 +0100

    Implement most of a Redis backend. Only method still missing is delete().

---
+++ spec/stores/redis_spec.js
@@ -0,0 +1,20 @@
+var Redis = require("../../lib/stores/redis"),
+    redis = require("redis")
+
+var db = redis.createClient(6379, "localhost")
+
+JS.Test.describe("Redis store", function() { with(this) {
+  before(function() { with(this) {
+    stub(require("../../lib/stores/core"), "hashRounds", 1)
+    this.store = new Redis({host: "localhost", port: 6379, database: 1})
+  }})
+  
+  after(function(resume) { with(this) {
+    db.select(1, function() {
+      db.flushdb(function() { resume() })
+    })
+  }})
+  
+  itShouldBehaveLike("storage backend")
+}})
+
