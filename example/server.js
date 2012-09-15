044ae31c99b7bd0ee2076157c668e97cb830f97a
James Coglan <jcoglan@gmail.com>
2012-09-15 13:39:22 +0100

    Add ability to run the example server using Redis.

--- example/server.js
+++ example/server.js
@@ -1,6 +1,12 @@
 var reStore = require('../lib/restore'),
-    store   = new reStore.File(__dirname + '/store'),
-    server  = new reStore({store: store});
+    store,
+    server;
 
+if (process.argv[3] === 'redis')
+  store = new reStore.Redis({database: 3});
+else
+  store = new reStore.File(__dirname + '/store');
+
+server = new reStore({store: store});
 server.listen(process.argv[2]);
 
