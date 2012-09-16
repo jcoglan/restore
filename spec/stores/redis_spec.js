6463c0156ec265c16bc909bb28980a54ccfffecb
James Coglan <jcoglan@gmail.com>
2012-09-16 10:53:53 +0100

    Extract parent-deleting code in Redis into a separate method, and fix a namespacing bug.

--- spec/stores/redis_spec.js
+++ spec/stores/redis_spec.js
@@ -6,7 +6,12 @@ var db = redis.createClient(6379, "localhost")
 JS.Test.describe("Redis store", function() { with(this) {
   before(function() { with(this) {
     stub(require("../../lib/stores/core"), "hashRounds", 1)
-    this.store = new Redis({host: "localhost", port: 6379, database: 1})
+    this.store = new Redis({
+      host:       "localhost",
+      port:       6379,
+      database:   1,
+      namespace:  String(new Date().getTime())
+    })
   }})
   
   after(function(resume) { with(this) {
