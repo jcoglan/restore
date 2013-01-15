c1cbef89d044a1e296d265167a6b6b4e878ec551
James Coglan <jcoglan@gmail.com>
2013-01-15 20:48:11 +0000

    Remove old File store option from example/server.js.

--- example/server.js
+++ example/server.js
@@ -6,10 +6,8 @@ var type = process.argv[2];
 
 if (type === 'redis')
   store = new reStore.Redis({database: 3});
-else if (type === 'tree')
-  store = new reStore.FileTree({path: __dirname + '/tree'});
 else
-  store = new reStore.File({path: __dirname + '/store'});
+  store = new reStore.FileTree({path: __dirname + '/tree'});
 
 server = new reStore({
   store:  store,
