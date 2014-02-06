c13f0583a3463898b6395c7a3159cf642be68fac
Michiel B. de Jong <michiel@unhosted.org>
2014-02-06 08:21:09 +0000

    fix webfinger

--- spec/runner.js
+++ spec/runner.js
@@ -2,13 +2,13 @@ process.env.SILENT = '1'
 
 var JS = require('jstest')
 
-//require('./restore/web_finger_spec')
+require('./restore/web_finger_spec')
 //require('./restore/oauth_spec')
 require('./restore/storage_spec')
 
-//require('./store_spec.js')
-//require('./stores/file_tree_spec')
-//require('./stores/redis_spec')
+require('./store_spec.js')
+require('./stores/file_tree_spec')
+require('./stores/redis_spec')
 
 JS.Test.autorun()
 
