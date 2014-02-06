a77575472896094bcccae0ca5dc479ca404ae97a
Michiel B. de Jong <michiel@unhosted.org>
2014-02-06 09:36:05 +0000

    fix OAuth post

--- spec/runner.js
+++ spec/runner.js
@@ -3,7 +3,7 @@ process.env.SILENT = '1'
 var JS = require('jstest')
 
 require('./restore/web_finger_spec')
-//require('./restore/oauth_spec')
+require('./restore/oauth_spec')
 require('./restore/storage_spec')
 
 require('./store_spec.js')
